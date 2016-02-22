bb6a78de8e5e26d500b1b8b8669841d4ecabdd25
Jeroen Schot <jeroen.schot@surfsara.nl>
2016-02-22 08:21:03 +0100

    Prepend to SPARK_*_OPTS instead of overwriting
    This fixes spark-shell which tries to add -usejavacp to
    SPARK_SUBMIT_OPTS.

--- conf/spark/spark-env.sh
+++ conf/spark/spark-env.sh
@@ -1,4 +1,4 @@
-spark_opts="-Djava.security.krb5.conf=${KRB5_CONFIG} -Djava.security.krb5.realm=CUA.SURFSARA.NL -Djava.security.krb5.kdc=kdc.hathi.surfsara.nl"
+spark_krb_opts="-Djava.security.krb5.conf=${KRB5_CONFIG} -Djava.security.krb5.realm=CUA.SURFSARA.NL -Djava.security.krb5.kdc=kdc.hathi.surfsara.nl"
 
-export SPARK_SUBMIT_OPTS="${spark_opts}"
-export SPARK_HISTORY_OPTS="${spark_opts}"
+export SPARK_SUBMIT_OPTS="${spark_krb_opts} ${SPARK_SUBMIT_OPTS}"
+export SPARK_HISTORY_OPTS="${spark_krb_opts} ${SPARK_HISTORY_OPTS}"
