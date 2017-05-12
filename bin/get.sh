a041bf1bba618384edf12a0b6eefc7d2cbee7468
Jeroen Schot <jeroen.schot@surfsara.nl>
2017-05-12 12:10:18 +0200

    Update Spark version to 2.1.1

--- bin/get.sh
+++ bin/get.sh
@@ -51,7 +51,7 @@ get_pig() {
 }
 
 get_spark() {
-  version=2.1.0
+  version=2.1.1
   url=http://www.eu.apache.org/dist/spark/spark-$version
   pkg=spark-$version-bin-without-hadoop
   tar="$pkg.tgz"
